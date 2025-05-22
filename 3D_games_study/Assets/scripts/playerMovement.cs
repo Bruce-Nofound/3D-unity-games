f63b3a84c40cdd692b7126836aa9d9776f24ad7e
Bruce-Nofound <etangsiyi@gmail.com>
2025-05-21 19:34:50 -0600

    OK

--- 3D_games_study/Assets/scripts/playerMovement.cs
+++ 3D_games_study/Assets/scripts/playerMovement.cs
@@ -3,11 +3,11 @@ using UnityEngine;
 
 public class playerMovement : MonoBehaviour
 {
-    public Transform CameraDiraction;
+    public GameObject CameraDiraction;
     public float rotationSpeed = 10f;
     void Start()
     {
-        CameraDiraction = GetComponent<Transform>();
+        CameraDiraction = GetComponent<GameObject>();
     }
 
     // Update is called once per frame
@@ -22,10 +22,8 @@ public class playerMovement : MonoBehaviour
     public void FollowCameraDiraction()
     {
         WaitForSeconds(0.1f);
-        Vector3 currentRotation = transform.eulerAngles;
-        float newY = Mathf.LerpAngle(currentRotation.y, CameraDiraction.eulerAngles.y, rotationSpeed * Time.deltaTime);
-        transform.rotation = Quaternion.Euler(0f, newY, 0f);
-        Debug.Log(CameraDiraction.eulerAngles.y);
+        transform.rotation = Quaternion.Euler(0f, CameraDiraction.transform.eulerAngles.y, 0f);
+        Debug.Log(CameraDiraction.transform.rotation);
         
     }
 
