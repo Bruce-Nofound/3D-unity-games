0dc37c29789732a3ac18f05e2d5aa8106ba15350
Bruce-Nofound <etangsiyi@gmail.com>
2025-05-22 18:27:41 -0600

    Update playerMovement.cs

--- 3D_games_study/Assets/scripts/playerMovement.cs
+++ 3D_games_study/Assets/scripts/playerMovement.cs
@@ -1,13 +1,23 @@
+﻿using JetBrains.Annotations;
 using System.Collections;
+using System.Collections.Generic;
 using UnityEngine;
 
 public class playerMovement : MonoBehaviour
 {
     public GameObject CameraDiraction;
+    public Animator anim;
     public float rotationSpeed = 10f;
+    public Dictionary<string, string> NinjiaAttack;
+    public string mudra;
     void Start()
     {
-        CameraDiraction = GetComponent<GameObject>();
+        CameraDiraction = GameObject.FindWithTag("MainCamera");
+        anim = GetComponent<Animator>();
+        NinjiaAttack = new Dictionary<string, string>()
+        {
+            {"","水龙弹"}
+        };
     }
 
     // Update is called once per frame
@@ -21,10 +31,48 @@ public class playerMovement : MonoBehaviour
     }
     public void FollowCameraDiraction()
     {
-        WaitForSeconds(0.1f);
-        transform.rotation = Quaternion.Euler(0f, CameraDiraction.transform.eulerAngles.y, 0f);
-        Debug.Log(CameraDiraction.transform.rotation);
-        
+        if (Input.GetKey(KeyCode.W))
+        {
+            WaitForSeconds(0.1f);
+            while (transform.eulerAngles.y != CameraDiraction.transform.eulerAngles.y)
+            {
+                float newY = Mathf.LerpAngle(transform.eulerAngles.y, CameraDiraction.transform.eulerAngles.y, rotationSpeed * Time.deltaTime);
+                transform.rotation = Quaternion.Euler(0f, newY, 0f);
+                Debug.Log(CameraDiraction.transform.rotation);
+                WaitForSeconds(Time.deltaTime);
+            }
+        }
     }
-
+    public void 结印()
+    {
+        if (Input.GetKey(KeyCode.LeftShift))
+        {
+            mudra = "";
+            while (Input.GetKeyDown(KeyCode.Q)==false)
+            {
+                if (Input.GetKey(KeyCode.Alpha1))
+                {
+                    mudra = mudra + "子";
+                    Debug.Log("子");
+                }
+                if (Input.GetKey(KeyCode.I))
+                {
+                    mudra = mudra + "丑";
+                }
+                if (Input.GetKey(KeyCode.O))
+                {
+                    mudra = mudra + "寅";
+                }
+                if (Input.GetKey(KeyCode.P))
+                {
+                    mudra = mudra + "卯";
+                }
+                if (Input.GetKey(KeyCode.Semicolon))
+                {
+                    mudra = mudra + "丑";
+                }
+            }
+            anim.SetTrigger("");
+        }
+    }
 }
