79a985c9d9e6604569c9afd500f9192c8c67b4c9
Bruce-Nofound <etangsiyi@gmail.com>
2025-05-24 10:06:10 -0600

    OK

---
+++ 3D_games_study/Assets/attacks.cs
@@ -0,0 +1,52 @@
+﻿using System.Collections.Generic;
+using UnityEngine;
+
+public class attacks : MonoBehaviour
+{
+    public string mudra;
+    public Animator anim;
+    public Dictionary<string, string> NinjiaAttack;
+    void Start()
+    {
+        anim = GetComponent<Animator>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+    public void 结印()
+    {
+        if (Input.GetKey(KeyCode.LeftShift))
+        {
+            mudra = "";
+            while (Input.GetKeyDown(KeyCode.Q) == false)
+            {
+                if (Input.GetKey(KeyCode.Alpha1))
+                {
+                    mudra = mudra + "子";
+                    Debug.Log("子");
+                }
+                if (Input.GetKey(KeyCode.I))
+                {
+                    mudra = mudra + "丑";
+                }
+                if (Input.GetKey(KeyCode.O))
+                {
+                    mudra = mudra + "寅";
+                }
+                if (Input.GetKey(KeyCode.P))
+                {
+                    mudra = mudra + "卯";
+                }
+                if (Input.GetKey(KeyCode.Semicolon))
+                {
+                    mudra = mudra + "丑";
+                }
+            }
+            anim.SetTrigger("");
+        }
+    }
+}
+
--- 3D_games_study/Assets/scripts/playerMovement.cs
+++ 3D_games_study/Assets/scripts/playerMovement.cs
@@ -1,13 +1,17 @@
 ﻿using JetBrains.Annotations;
 using System.Collections;
 using System.Collections.Generic;
+using Unity.VisualScripting;
+using UnityEditor.Experimental.GraphView;
 using UnityEngine;
 
 public class playerMovement : MonoBehaviour
 {
-    public GameObject CameraDiraction;
     public Animator anim;
     public Rigidbody rb;
+    public Transform orientation;
+    public Transform player;
+    public Transform playerobj;
     public float rotationSpeed = 10f;
     public Dictionary<string, string> NinjiaAttack;
     public string mudra;
@@ -16,20 +20,21 @@ public class playerMovement : MonoBehaviour
     public Vector3 MovementInput;
     void Start()
     {
-        CameraDiraction = GameObject.FindWithTag("MainCamera");
         rb = GetComponent<Rigidbody>();
         anim = GetComponent<Animator>();
         NinjiaAttack = new Dictionary<string, string>()
         {
             {"","水龙弹"}
         };
+        Cursor.lockState = CursorLockMode.Locked;
+        Cursor.visible = false;
     }
 
     // Update is called once per frame
     void Update()
     {
         FollowCameraDiraction();
-        结印();
+        
     }
     IEnumerator WaitForSeconds(float seconds)
     {
@@ -37,54 +42,15 @@ public class playerMovement : MonoBehaviour
     }
     public void FollowCameraDiraction()
     {
-        if (Input.GetKey(KeyCode.W))
+        Vector3 veiwDiraction = player.position - new Vector3(transform.position.x, player.position.y, transform.position.z);
+        orientation.forward = veiwDiraction.normalized;
+        float Xinput = Input.GetAxis("Horizontal");
+        float Zinput = Input.GetAxis("Vertical");
+        Vector3 inputDir = orientation.forward * Zinput + orientation.right * Xinput;
+        if (inputDir != Vector3.zero)
         {
-            WaitForSeconds(0.1f);
-            float newY = Mathf.LerpAngle(transform.eulerAngles.y, CameraDiraction.transform.eulerAngles.y, rotationSpeed * Time.deltaTime);
-            transform.rotation = Quaternion.Euler(0f, newY, 0f);
-            Debug.Log(CameraDiraction.transform.rotation);
-            WaitForSeconds(Time.deltaTime);
+            player.forward = Vector3.Slerp(transform.forward, inputDir, rotationSpeed * Time.deltaTime);
         }
-        if (Input.GetKey(KeyCode.W))
-        {
-            MovementInput.x = Input.GetAxis("Horizontal");
-            MovementInput.z = Input.GetAxis("Vertical");
-        }
-        else
-        {
-            rb.linearVelocity = Vector3.zero;
-        }
-    }
-    public void 结印()
-    {
-        if (Input.GetKey(KeyCode.LeftShift))
-        {
-            mudra = "";
-            while (Input.GetKeyDown(KeyCode.Q)==false)
-            {
-                if (Input.GetKey(KeyCode.Alpha1))
-                {
-                    mudra = mudra + "子";
-                    Debug.Log("子");
-                }
-                if (Input.GetKey(KeyCode.I))
-                {
-                    mudra = mudra + "丑";
-                }
-                if (Input.GetKey(KeyCode.O))
-                {
-                    mudra = mudra + "寅";
-                }
-                if (Input.GetKey(KeyCode.P))
-                {
-                    mudra = mudra + "卯";
-                }
-                if (Input.GetKey(KeyCode.Semicolon))
-                {
-                    mudra = mudra + "丑";
-                }
-            }
-            anim.SetTrigger("");
-        }
+
     }
 }
